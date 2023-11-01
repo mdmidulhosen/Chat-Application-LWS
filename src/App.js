d11529f9b53cee2bd61b092985ec3afb0d368968
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-01 16:40:03 +0600

    public routing and private routing is done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import useAuthCheck from "./Hooks/useAuthCheck";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
   const authChecked = useAuthCheck();
@@ -12,10 +14,10 @@ function App() {
   ) : (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/inbox" element={<Conversation />} />
-        <Route path="/inbox/:id" element={<Inbox />} />
+        <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+        <Route path="/inbox" element={<PrivateRoute><Conversation /></PrivateRoute>} />
+        <Route path="/inbox/:id" element={<PrivateRoute><Inbox /></PrivateRoute>} />
       </Routes>
     </Router>
   );
