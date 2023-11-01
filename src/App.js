d36a197940f9d4497b4d5c846f129b5a205718dc
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-01 16:06:38 +0600

    authantication checking from localstorage

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import useAuthCheck from "./Hooks/useAuthCheck";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
-    );
+  const authChecked = useAuthCheck();
+  return !authChecked ? (
+    <div>Authantication Checking...</div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
