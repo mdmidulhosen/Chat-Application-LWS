705680884b3831b3a9a026811dc123b2eb86b3d9
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-04 09:16:45 +0600

    authanticate request problem has been fixed

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 export default function useAuth() {
     const auth = useSelector((state) => state.auth)
 if(auth?.accessToken && auth.user){
+  console.log("useAuth: ", auth)
     return true
   }else{
     return false
